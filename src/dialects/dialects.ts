fcf529508495fc78c1c2e02929e7dd1af3e3ec79
octane92 <oct4ne92@gmail.com>
2026-01-31 12:19:14 +0100

    removed not implemented type

--- src/dialects/dialects.ts
+++ src/dialects/dialects.ts
@@ -4,15 +4,11 @@ import { postgresOptions } from './postgres';
 import { sqliteOptions } from './sqlite';
 import { DialectOptions } from './dialect-options';
 import { Dialect } from 'sequelize';
-import { NotImplementedOptions } from './not-implemented';
 
 export const dialects: { [name in Dialect]: DialectOptions } = {
   mssql: mssqlOptions,
   mysql: mysqlOptions,
   mariadb: mysqlOptions,
   postgres: postgresOptions,
-  sqlite: sqliteOptions,
-  db2: NotImplementedOptions,
-  snowflake: NotImplementedOptions,
-  oracle: NotImplementedOptions
+  sqlite: sqliteOptions
 };
